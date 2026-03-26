be2aa1602a68380b0e161a7a840560475af473b4
wikeleke <uliseselias@gmail.com>
2026-03-26 10:15:21 -0600

    Fix Mongoose 9 next() callback error in pre-save hook

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -26,11 +26,10 @@ const UserSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Hash password before saving
-UserSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
+UserSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
 UserSchema.methods.comparePassword = async function (candidatePassword) {
